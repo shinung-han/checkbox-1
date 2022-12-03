6145f1f26a4b4d677f594897d5d7c51c853ee84e
shinung-han <hanshinung@gmail.com>
2022-12-03 14:20:04 +0900

    checkbox-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [checked, setChecked] = useState({
+    age: true,
+    name: true,
+    membership: true,
+  });
+
+  const { age, name, membership } = checked;
+
+  const handleCheck = (e) => {
+    const { name } = e.target;
+    setChecked({ ...checked, [name]: !checked[name] });
+  };
+
+  // Object.entries & reduce 사용
+  const handleAllCheck = () => {
+    // const checkValue = isAllCheked ? false : true;
+    setChecked(
+      Object.entries(checked).reduce(
+        (acc, [key]) => ({ ...acc, [key]: !isAllCheked }),
+        {}
+      )
+    );
+  };
+
+  // for in 반복문 사용
+  //   const handleAllCheck = () => {
+  //     let newObj = {};
+  //     for (let key in checked) {
+  //       newObj = { ...newObj, [key]: !isAllCheked };
+  //     }
+  //     setChecked(newObj);
+  //   };
+
+  const isAllCheked = Object.values(checked).every((item) => item === true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input
+          type="checkbox"
+          checked={isAllCheked}
+          onChange={handleAllCheck}
+        />
+        전체선택
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          name="age"
+          checked={age}
+          onChange={handleCheck}
+        />
+        <label htmlFor="">age</label>
+        <input
+          type="checkbox"
+          name="name"
+          checked={name}
+          onChange={handleCheck}
+        />
+        <label htmlFor="">name</label>
+        <input
+          type="checkbox"
+          name="membership"
+          checked={membership}
+          onChange={handleCheck}
+        />
+        <label htmlFor="">membership</label>
+      </div>
     </div>
   );
 }
-
-export default App;
